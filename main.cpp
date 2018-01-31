564769253321aa7af0034607c8b12461cf2da387
moaazmagdy6 <35852130+moaazmagdy6@users.noreply.github.com>
2018-01-31 23:58:15 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Node.cpp"
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	Node n1('c');
+	cout << n1.getNodeName() << endl;
+	
+	
+	return 0;
+}
